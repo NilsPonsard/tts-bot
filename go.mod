c7cfb503d575b61bf25b0045f25c5713d8b3f2aa
Nils Ponsard <nilsponsard@gmail.com>
2021-10-16 11:54:39 +0200

    tts test

--- go.mod
+++ go.mod
@@ -5,5 +5,9 @@ go 1.16
 require (
 	github.com/TwinProduction/go-color v1.0.0
 	github.com/bwmarrin/discordgo v0.23.2
+	github.com/hegedustibor/htgo-tts v0.0.0-20210901072303-b76f41971260
 	github.com/jawher/mow.cli v1.2.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 )
--- internal/commands/start/start.go
+++ internal/commands/start/start.go
@@ -2,9 +2,17 @@ package start
 
 import (
 	"github.com/bwmarrin/discordgo"
+	htgotts "github.com/hegedustibor/htgo-tts"
 	cli "github.com/jawher/mow.cli"
 	"github.com/nilsponsard/tts-bot/pkg/verbosity"
 )
+
+type fileHandler struct{}
+
+func (*fileHandler) Play(filename string) error {
+	verbosity.Info(filename)
+	return nil
+}
 
 // setup ping command
 func Start(job *cli.Cmd) {
@@ -12,6 +20,15 @@ func Start(job *cli.Cmd) {
 	// function to execute
 
 	job.Action = func() {
+
+		speech := htgotts.Speech{Folder: "audio", Language: "en", Handler: &fileHandler{}}
+
+		err := speech.Speak("test")
+
+		if err != nil {
+			verbosity.Error(err)
+		}
+
 		discord, err := discordgo.New("Bot " + "authentication token")
 		if err != nil {
 			verbosity.Error(err)
